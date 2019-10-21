895bbf2fca3de5f5b1323995b67959a5bc827300
timzaitsev <tim.zaitsev@yandex.ru>
2019-10-21 18:14:14 +0300

    Сделал задания 1- 3 по работе 3

---
+++ lesson_3.py
@@ -0,0 +1,40 @@
+#-------------------Третье задание-------------------
+print('Задание 3')
+def sum_to_max():
+    try:
+        a_1 = float(input('Введите первое число '))
+        a_2 = float(input('Введите второе число '))
+        a_3 = float(input('Введите третье число '))
+    except ValueError:
+        print('Введено некорректное значение')
+        return
+    list = [a_1, a_2, a_3]
+    a = min(list)
+    if list.index(a) == 0:
+        max_2_sum = a_2 + a_3
+    elif list.index(a) == 1:
+        max_2_sum = a_1 + a_3
+    elif list.index(a) == 2:
+        max_2_sum = a_1 + a_2
+    return max_2_sum
+print(sum_to_max())
+#-------------------Второе задание-------------------
+print('Задание 2')
+def card(**kwargs):
+    return kwargs
+print(card(Имя = input('Введите имя '), Фамилия = input('Введите фамилию '), Дата_рождения = input('Введите дату рождения '), Город = input('Введите город проживания '), email = input('Введите email '), phone = input('Введите омер телефона ')))
+#-------------------Первое задание-------------------
+print('Задание 1')
+def divide(dividend, divider):
+    try:
+        private = float(dividend) / float(divider)
+    except ZeroDivisionError:
+        print('Ошибка деления на ноль')
+    except ValueError:
+        print('Введено некорректное значение')
+        return
+    return private
+
+arg_1 = input('введите делимое ')
+arg_2 = input('введите делитель ')
+print(divide(arg_1,arg_2))
